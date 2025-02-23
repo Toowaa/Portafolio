b5f6e9ba1eef261fa92a9c8bccf632e02583357a
Toowaa <brahanax@gmail.com>
2025-02-23 13:10:05 -0500

    feat:Letters

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import * as React from "react";
+
+// 1. import `HeroUIProvider` component
+import {HeroUIProvider} from "@heroui/system";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +28,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es" >
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <HeroUIProvider>
         {children}
+    </HeroUIProvider>
+  
       </body>
     </html>
   );
---
+++ components/textver1.tsx
@@ -0,0 +1,80 @@
+import { motion } from "framer-motion";
+
+export const Textver1 = () => {
+  return (
+    <section
+      className="grid h-screen 
+    place-content-center gap-2 bg-green-300 px-9 text-black
+    "
+    >
+      <FlipLink href="#">Twitter</FlipLink>
+      <FlipLink href="#">Linkedln</FlipLink>
+      <FlipLink href="#">Facebook</FlipLink>
+      <FlipLink href="#">Instragram</FlipLink>
+    </section>
+  );
+};
+
+const DURATION = 0.25;
+const STAGGER = 0.050;
+
+const FlipLink = ({ children, href }: { children: string; href: string }) => {
+  return (
+    <motion.a
+      initial="initial"
+      whileHover="hovered"
+      href={href}
+      className="relative block overflow-hidden whitespace-nowrap text-4xl font-black uppercase sm:text-7xl md:text-8xl lg:text-9xl"
+      style={{
+        lineHeight: 0.75,
+      }}
+    >
+      <div>
+        {children.split("").map((l, i) => (
+          <motion.span
+            variants={{
+              initial: {
+                y: 0,
+              },
+              hovered: {
+                y: "-110%",
+              },
+            }}
+            transition={{
+              duration: DURATION,
+              ease: "easeInOut",
+              delay: STAGGER * i,
+            }}
+            className="inline-block"
+            key={i}
+          >
+            {l}
+          </motion.span>
+        ))}
+      </div>
+      <div className="absolute inset-0">
+        {children.split("").map((l, i) => (
+          <motion.span
+            variants={{
+              initial: {
+                y: "100%",
+              },
+              hovered: {
+                y: 0,
+              },
+            }}
+            transition={{
+              duration: DURATION,
+              ease: "easeInOut",
+              delay: STAGGER * i,
+            }}
+            className="inline-block"
+            key={i}
+          >
+            {l}
+          </motion.span>
+        ))}
+      </div>
+    </motion.a>
+  );
+};
